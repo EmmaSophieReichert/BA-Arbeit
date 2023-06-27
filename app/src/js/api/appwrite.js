1c2138e7775601815e270bb85c61637c455297f7
EmmaSophieReichert <emmasophie1203@gmx.de>
2023-06-27 17:51:41 +0200

    Appwrite Create Account

--- app/src/js/api/appwrite.js
+++ app/src/js/api/appwrite.js
@@ -1,7 +1,18 @@
-const { Client, Account, ID } = Appwrite;
-const appwrite = new Client();
+const { Client, Account, ID, Databases } = Appwrite;
+// const appwrite = new Client();
 
-appwrite
+const client = new Client(),
+     account = new Account(client),
+     database = new Databases(client);
+
+const appwrite = {
+     client: client,
+     account: account,
+     database: database,
+     ID: ID,
+}
+
+appwrite.client
      .setEndpoint("https://cloud.appwrite.io/v1") // Your Appwrite Endpoint process.env.APPWRITE_ENDPOINT
      .setProject("649ab3887121a935da21"); // Your project ID process.env.APPWRITE_KEY
      
---
+++ app/src/js/utils/Config.js
@@ -0,0 +1,12 @@
+/* eslint-env browser */
+
+// Configuration object for values shared by multiple components
+
+const Config = {
+    DATABASE_ID: "649afbb84bb08d4663e1",
+    COLLECTION_ID: "649b037f907ae057e75a",
+};
+
+Object.freeze(Config);
+
+export default Config;
