2a3aa1537d1fa26bd63ee4eef1c1842f62238e4d
EmmaSophieReichert <emmasophie1203@gmx.de>
2023-07-29 16:04:00 +0200

    Fixing cors policy problem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import open from "open";
 
-const HTTP_PORT = 8080;
+const HTTP_PORT = 8000;
 
 var app;
 
@@ -11,7 +11,7 @@ function init() {
     app.use("/", express.static("app"));
     app.listen(HTTP_PORT, function() {
         console.log("Server started. Opening application in browser ... [Press CTRL + C to stop server]");
-        open("http://localhost:8080");
+        open("http://localhost:8000");
     });
 }
 
