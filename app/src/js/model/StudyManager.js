6df477bee2cc4c979606ce3e422e0c6bd12e3b2c
EmmaSophieReichert <emmasophie1203@gmx.de>
2023-08-05 10:16:52 +0200

    fix create study bug

--- app/src/js/model/StudyManager.js
+++ app/src/js/model/StudyManager.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 
+import { getAuth } from "../api/Auth/getAuth.js";
 import { createFile } from "../api/Storage/createFile.js";
 import { Observable, Event } from "../utils/Observable.js";
 import Studies from "./structure/Studies.js";
@@ -24,11 +25,19 @@ class StudyManager extends Observable {
             studies = new Studies(data.degree, data.ects, semesters, data.subjects, data.specializations);
         let studyJSON = JSON.stringify(studies),
             blob = new Blob([studyJSON], { type: "text/plain" }),
-            file = new File([blob], studies.subjects[0].title);
-        await createFile(file).then(() => { 
-            this.notifyAll(new Event("studies-reached-cloud", "studies reached cloud")); 
-            console.log("Studies reached cloud StudyManager");
-        });
+            file = new File([blob], studies.subjects[0].title),
+            auth = await getAuth();
+        if(auth.login){
+            let promise = createFile(file, auth);
+            await promise.then(() => { 
+                this.notifyAll(new Event("studies-reached-cloud", "studies reached cloud")); 
+                console.log("Studies reached cloud StudyManager");
+            });
+        }
+        else{
+            window.location.hash = "login";
+            location.reload();
+        }
     }
 
 }
